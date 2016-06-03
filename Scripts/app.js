e6f792e4d518258100a5061a251f992abe73974d
Web1017 <j-n_01@hotmail.com>
2016-06-02 20:13:44 -0400

    Added Project Info and Icons

--- Scripts/app.js
+++ Scripts/app.js
@@ -10,6 +10,7 @@
     paragraphElements[1] = document.getElementById("ParagraphTwo");
     paragraphElements[2] = document.getElementById("ParagraphThree");
     paragraphElements[3] = document.getElementById("ParagraphFour");
+    paragraphElements[4] = document.getElementById("ParagraphFive");
 
     var paragraphs = [];
 
@@ -17,10 +18,14 @@
     paragraphs[0] = "Life is a series of experiences, each one of which makes us bigger, even though sometimes it is hard to realize this. For the world was built to develop character, and we must learn that the setbacks and grieves which we endure help us in our marching onward. - Henry Ford";
 
    
-    paragraphs[1] = "Second step of the sentence. You can view what will be added to this page, and see the new features";
+    paragraphs[1] = "<span class = 'firstSentence' >Study of history has always been a passion of mine. Studies from Ancient Egypt to Political systems, has always brought me interest. One of my recent project that I have worked on is the study of Latin and how it had dominated the European language for centuries.</span>";
+    
     paragraphs[2] = "My personal goal is to gain more knowledge and experience in life in order to succeed. With more interest and practise, I hope to gain more than before. My personal ambition has shown me that I am capable of doing more with my life and being able to expand my horizon in terms of knowledge and skills, which will guide me toward my future ";
     
-    paragraphs[3] = "my favorite market";
+    paragraphs[3] = "<span class = 'firstSentence' >Study of the economy and market which dominate this world has been one of my personal interests. Commodity which sets the structure of how modern economy function, has been one of my personal interest which I persue to gain and understand.</span>";
+    
+    paragraphs[4] = "<span class = 'firstSentence' >One of My favorite projects was programming and creating websites. Creating a website as well as creating applications using C# has been a memorable time. Using programming tools to create my ideas and share it with others.</span> "
+    
 
     //check if paragraph exists   
 
